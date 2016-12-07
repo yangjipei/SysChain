13b05464ac758189f43ba6ae20c918750c0fbb2a
Michael Yang <Michael@Michaels-Pro.lan>
2016-12-07 18:38:56 +0800

    Model验证

--- SysChain/Areas/Admin/ViewModels/LoginModel.cs
+++ SysChain/Areas/Admin/ViewModels/LoginModel.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace SysChain.Admin
 {
 	public class LoginModel
 	{
+		[Required(ErrorMessage ="登录账号不能为空")]
 		public string LoginName { set; get; }
+		[Required(ErrorMessage = "登录密码不能为空"),MinLength(6,ErrorMessage="密码不能小于6位字符")]
+		[DataType(DataType.Password)]
 		public string LoginPassword { set; get; }
 	}
 }
